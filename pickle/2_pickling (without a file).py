3320aa009d86dd01b87142ec884c9f66ff414844
Khailas R <khailas303@gmail.com>
2021-09-26 14:12:50 +0530

    update

--- pickle/2_pickling (without a file).py
+++ pickle/2_pickling (without a file).py
@@ -16,7 +16,9 @@ db = {
 }
 
 pickling = pickle.dumps(db)
-
 my_entry = pickle.loads(pickling)
 
 print(my_entry)
+
+
+# Pickle in Python is primarily used in serializing and deserializing a Python object structure. In other words, it's the process of converting a Python object into a byte stream to store it in a file/database, maintain program state across sessions, or transport data over the network.
