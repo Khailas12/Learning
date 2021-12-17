06e2a7ba44503120d827d853ef14bc7333ffe442
Khailas R <khailas303@gmail.com>
2021-12-17 17:26:14 +0530

    intital commit

---
+++ Problem Solving/Algos based/avg word len.py
@@ -0,0 +1,14 @@
+first = 'Batman is one of the greatest superheros of all time'
+second = 'Gotham is a fictional city'
+
+
+def avg_word(sentence):
+    for x in "!?',;.":
+        sentence = sentence.replace(x, '')
+        
+    words = sentence.split()
+    return round(sum(len(word) for word in words)/len(words), 2)
+
+
+print(avg_word(first))
+print(avg_word(second))
