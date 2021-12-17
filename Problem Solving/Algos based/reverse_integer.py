a846f8ec8b89a8ed18d218cd9fce813a2e93be98
Khailas R <khailas303@gmail.com>
2021-12-17 17:26:24 +0530

    updated

--- Problem Solving/Algos based/reverse_integer.py
+++ Problem Solving/Algos based/reverse_integer.py
@@ -2,13 +2,12 @@
 
 def reverse(x):
     string = str(x)
-
+    
     if string[0] == '-':
-        return int(f"-{string[:0:-1]}")
-
+        return int(f'-{string[:0:-1]}')
+    
     else:
         return int(f'{string[::-1]}')
-
-
-print(reverse(-321))
-print(reverse(456))
+    
+print(reverse(-473))
+print(reverse(431))
