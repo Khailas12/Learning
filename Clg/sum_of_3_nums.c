481555dd1f5c79c33dc35c1023ecbcd7cd7366b8
Khailas R <khailas303@gmail.com>
2022-01-20 20:52:30 +0530

    initial commit

---
+++ Clg/sum_of_3_nums.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+
+{
+    int x, y, z, sum;
+    printf('Enter 3 nums: ');
+    scanf('  %d%d%d', &x, &y, &z);
+
+    sum = x + y + z;
+    printf('The sum = %d', sum);
+}
