3e67830b2145b122aa0dea740772a625a79679e5
Khailas R <khailas303@gmail.com>
2022-02-04 00:44:44 +0530

    initial commit

---
+++ Java/8_Reads_a_file_and_displays_the_file.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Rfile {
+    public static void main(String[] args) {
+        int j = 1;
+        char ch;
+
+        Scanner Scr = new Scanner(System.in);
+        System.out.println("Enter the file");
+        String Str = Scr.next();
+
+        FileInputStream f = new FileInputStream(Str);
+        System.out.println("Contents of the file are");
+
+        int n = f.available();
+        System.out.println(j + ": ");
+
+        for (int i = 1; i < n; i++) {
+            ch = (char) f.read();
+            System.out.println(ch);
+            if (ch == '\n') {
+                System.out.println(++j + ": ");
+            }
+        }
+    }
+}
