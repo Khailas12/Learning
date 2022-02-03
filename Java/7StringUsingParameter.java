81997238d851d9db674402be03f785d39f234c92
Khailas R <khailas303@gmail.com>
2022-02-03 23:38:57 +0530

    updated

--- Java/7StringUsingParameter.java
+++ Java/7StringUsingParameter.java
@@ -1,26 +1,30 @@
 import java.awt.*;
 import java.applet.*;
 
-
 class font extends Applet {
     Font f1, f2, f3;
 
     public void init() {
-        f1 = new Font("Arial", Font.BOLD, 18);
-        f2 = new Font("Forte", Font.PLAIN, 24);
-        f3 = new Font("Elephant", Font.ITALIC, 29);
+        f1 = new Font("Arial", Font.BOLD, 23);
+        f2 = new Font("Forte", Font.PLAIN, 18);
+        f3 = new Font("Elephant", Font.ITALIC, 23);
     }
 
     public void paint(Graphics g) {
-        g.drawString("Java Programming", 50, 50);
+        g.drawString("Java Programming", 45, 45);
         g.setFont(f1);
 
-        g.drawString("Ada", 45, 35);
+        g.drawString("Ada", 50, 80);
         g.setFont(f2);
 
-        g.drawString("AI", 34, 43);
+        g.drawString("Artificial Intelligence", 50, 110);
         g.setFont(f3);
 
-        g.drawString("DCN", 35, 45);
+        g.drawString("DCN", 50, 140);
     }
 }
+
+/*
+ * <applet code="font.class" height=500 width=500>
+ * </applet>
+ */
