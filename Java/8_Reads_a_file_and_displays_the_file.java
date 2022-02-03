63426ec71d0482e9315155b637aaae899d9d37ef
Khailas R <khailas303@gmail.com>
2022-02-04 00:58:04 +0530

    bug fix

--- Java/8_Reads_a_file_and_displays_the_file.java
+++ Java/8_Reads_a_file_and_displays_the_file.java
@@ -3,15 +3,16 @@ import java.io.*;
 
 class Rfile {
     public static void main(String args[]) throws IOException {
+
         int j = 1;
         char ch;
 
         Scanner Scr = new Scanner(System.in);
-        System.out.println("Enter the file");
+        System.out.println("Enter the file: ");
         String Str = Scr.next();
 
         FileInputStream f = new FileInputStream(Str);
-        System.out.println("Contents of the file are");
+        System.out.println("\nContents of the files are");
 
         int n = f.available();
         System.out.println(j + ": ");
@@ -20,7 +21,7 @@ class Rfile {
             ch = (char) f.read();
             System.out.println(ch);
             if (ch == '\n') {
-                System.out.println(++j + ": ");
+                System.out.println(j + ": ");
             }
         }
     }
