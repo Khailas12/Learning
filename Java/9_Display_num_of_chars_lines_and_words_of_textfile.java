858ddbfe6e9442af83504b581e290ef8bf1fd3f1
Khailas R <khailas303@gmail.com>
2022-02-04 01:53:52 +0530

    initial commit

---
+++ Java/9_Display_num_of_chars_lines_and_words_of_textfile.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+
+class Cfile {
+    public static void main(String args[]) throws IOException {
+        int nl = 1, nw = 0;
+        char ch;
+
+        Scanner Scr = new Scanner(System.in);
+        System.out.println("Enter the file: ");
+        String Str = Scr.nextLine();
+
+        FileInputStream f = new FileInputStream(Str);
+        int n = f.available();
+
+        for (int i = 0; i < n; i++) {
+            ch = (char) f.read();
+
+            if (ch == '\n') {
+                nl++;
+            } 
+            else if (ch == ' ') {
+                nw++;
+            }
+        }
+
+        System.out.println("\nNumber of Lines: " + nl);
+        System.out.println("\nNumber of Words: " + (nl + nw));
+        System.out.println("\nNumber of Characters: " + n);
+    }
+}
