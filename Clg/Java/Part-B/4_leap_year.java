ff1be652f613f9c72ef4dbb33d29afaa1216508b
Khailas R <khailas303@gmail.com>
2022-02-24 00:09:21 +0530

    initial commit

---
+++ Clg/Java/Part-B/4_leap_year.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class LeapYear {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(system.in));
+
+        System.out.println("Enter Year");
+        int year = Integer.parseInt(br.readLine());
+
+        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println("Year" + year + " is a leap year");
+        }
+        else {
+            System.out.println("year" + year + "us not a leap year");
+        }
+    }
+}
